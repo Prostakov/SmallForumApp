591d8e0a3e160f882c9b55ab3b30cfe296cb096d
Serhii Prostakov <s.prostakov@yandex.com>
2017-11-19 01:08:05 +0200

    Started fixing crud for users

---
+++ TestAuthorizationApp/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TestAuthorizationApp.Models;
+
+namespace TestAuthorizationApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Users = _userManager.Users.ToList();
+            return View();
+        }
+    }
+}
