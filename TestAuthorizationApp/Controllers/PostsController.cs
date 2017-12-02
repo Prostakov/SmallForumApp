188184be376b703f07fca2c584c0d8a960a75cd6
Serhii Prostakov <s.prostakov@yandex.com>
2017-12-02 22:43:07 +0200

    Added PostsController

---
+++ TestAuthorizationApp/Controllers/PostsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAuthorizationApp.Data;
+
+namespace TestAuthorizationApp.Controllers
+{
+    public class PostsController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
